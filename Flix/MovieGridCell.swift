355140e6ab2c6da5bfb36d5a301e2064391684fa
jpm20e <88345917+jpm20e@users.noreply.github.com>
2021-09-12 18:38:32 -0400

    Initial commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Josh Minucci on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
